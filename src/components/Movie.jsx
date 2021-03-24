5113969c72c3baaae2eb19b8c3e937281fc18408
Susanna Pezzini <susanna.pezzini@gmail.com>
2021-03-24 18:53:46 +0000

    simple routing conquered

---
+++ src/components/Movie.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './Flat.scss'
+
+const Movie = () => {
+  return (
+    <div>
+     <h1>This is the movie page</h1>
+    </div>
+  );
+}
+
+export default Movie;
