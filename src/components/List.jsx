b29a14683e025b2112807fff5cd68ab71180cb25
Susanna Pezzini <susanna.pezzini@gmail.com>
2021-03-25 12:11:23 +0000

    displaying a damn icon from fontawesome, damn react

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,27 +1,52 @@
 import React from 'react';
 import '../styles/List.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
 
-const MOVIES_URL = 'http://www.omdbapi.com/?s=titanic&apikey=d885f351'
+const MOVIES_URL = 'http://www.omdbapi.com/?s=2021&apikey=d885f351'
 
 class List extends React.Component {
   state = {
-    movies: []
+    movies: [],
+    filterText: ''
   }
   componentDidMount() {
     setTimeout(() => {
       fetch(MOVIES_URL)
         .then(response => response.json())
-        .then(data => this.setState({ movies: data }));
+        .then(data => this.setState({movies: data.Search}));
     }, 1000);
   }
+
+  handleFilter = (event) => {
+    const text = event.target.value;
+    this.setState({ filterText: text });
+    console.log(this.state.filterText);
+  }
+
   render() {
-    const { movies } = this.state;
-    console.log(movies);
+    const { movies, filterText } = this.state;
+    let movieList = '';
+    if (filterText === '') {
+      movieList = 
+      <div className="empty">
+        {/* <i class="fas fa-lightbulb"></i> */}
+        <FontAwesomeIcon icon={faLightbulb} />
+        {/* <FontAwesomeIcon icon={['fab', 'google']} /> */}
+        <p className="empty__claim">Don't know what to search?</p>
+        <p>Here's an offer you can't refuse</p>
+      </div>
+    } else {
+      movieList = <div>{filterText}</div>
+    }
     return (
     <div className="container">
-    <input className="search" onChange={this.handleFilter} placeholder="Search Movies..."/>
-     <h1>{movies[0]}</h1>
+      <input className="search" onChange={this.handleFilter} placeholder="Search Movies..."/>
+      
+        {movieList}
+  
     </div>
+    
     );
   }
 }
