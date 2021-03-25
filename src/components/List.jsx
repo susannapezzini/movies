b72625aa2f4c3b756b1d4badd794f284e9461489
Susanna Pezzini <susanna.pezzini@gmail.com>
2021-03-25 13:16:49 +0000

    the search is working in a sketchy way

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,47 +4,66 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
 
 const MOVIES_URL = 'http://www.omdbapi.com/?s=2021&apikey=d885f351'
+const API = 'd885f351'
+
 
 class List extends React.Component {
   state = {
     movies: [],
-    filterText: ''
-  }
-  componentDidMount() {
-    setTimeout(() => {
-      fetch(MOVIES_URL)
-        .then(response => response.json())
-        .then(data => this.setState({movies: data.Search}));
-    }, 1000);
+    filterText: '', 
+    error: ''
   }
 
   handleFilter = (event) => {
     const text = event.target.value;
     this.setState({ filterText: text });
-    console.log(this.state.filterText);
+  }
+
+  handleSearch = (event) => {
+    const { filterText, movies} = this.state;
+    if (event.key === 'Enter') {
+      // console.log(filterText);
+      const url = `http://www.omdbapi.com/?s=${this.state.filterText}&apikey=${API}`;
+      // console.log(url);
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log('data', data.Search );
+        if (data.Search) {
+          this.setState({movies: data.Search});
+        } else {
+          this.setState({movies: -1 });
+          this.setState({error: 'Sorry we could not find what you were looking for, try again!'});
+        }
+      });
+       
+    }
   }
 
   render() {
-    const { movies, filterText } = this.state;
-    let movieList = '';
-    if (filterText === '') {
+    const { movies, filterText, error } = this.state;
+    console.log(movies, movies.length);
+    let movieList = '';  
+    if (movies.length === 0) {
       movieList = 
       <div className="empty">
-        {/* <i class="fas fa-lightbulb"></i> */}
         <FontAwesomeIcon icon={faLightbulb} />
-        {/* <FontAwesomeIcon icon={['fab', 'google']} /> */}
         <p className="empty__claim">Don't know what to search?</p>
         <p>Here's an offer you can't refuse</p>
-      </div>
+      </div>; 
+    } else if (movies === -1) {
+      movieList = 
+      <p className="error">{error}</p>
     } else {
-      movieList = <div>{filterText}</div>
+      movieList = 
+        <div className="movies">
+            <h1>Here is your search</h1>
+        </div>;
     }
     return (
     <div className="container">
-      <input className="search" onChange={this.handleFilter} placeholder="Search Movies..."/>
-      
-        {movieList}
-  
+      <input className="search" onChange={this.handleFilter} onKeyDown={this.handleSearch} placeholder="Search Movies..."/>
+      {movieList}
     </div>
     
     );
